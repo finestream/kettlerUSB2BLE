3a751e4fe8e392131d3b1c17a7f56f147c348b6e
Bohdan Bashynskyi <bohdan@intergrated-digital.com>
2026-01-04 02:56:09 +0100

    Add Bleno HCI initialization logging to KettlerBLE class

--- BLE/kettlerBLE.js
+++ BLE/kettlerBLE.js
@@ -73,6 +73,10 @@ class KettlerBLE extends EventEmitter {
 			console.log(`[${this.name} rssiUpdate]: ${rssi}`);
 		});
 
+		// Initialize Bleno HCI
+		console.log(`[${this.name}] Initializing Bleno HCI...`);
+		bleno.init();
+
 	}
 
 	// notifiy BLE services
